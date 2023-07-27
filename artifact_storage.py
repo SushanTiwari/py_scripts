f1cc2a493d3dae94c00c88610af9591f5741cad6
Sushan Tiwari <sushan.tiwari@totalexpert.com>
2023-07-27 15:15:26 -0400

    added local and stage

--- artifact_storage.py
+++ artifact_storage.py
@@ -3,6 +3,11 @@ import random
 import string
 import requests
 
+# Global variables for URL and verification
+BASE_URL = "http://localhost:5001"
+#BASE_URL = "https://artifact-storage.staging.totalexpert.io"
+VERIFY_CERTIFICATE = False  # Set to True if you want to verify SSL certificates, False to disable verification
+
 # Function to generate a random filename
 def generate_random_filename():
     letters = string.ascii_lowercase
@@ -33,8 +38,8 @@ def generate_random_string(length):
 
 # Function to perform the HTTP POST request and extract "uploadUrl" from JSON response
 def perform_post_request(filename):
-    url = f"https://artifact-storage.staging.totalexpert.io/v1/artifact/a?filename={filename}"
-    response = requests.post(url, data='')
+    url = f"{BASE_URL}/v1/artifact/a?filename={filename}"
+    response = requests.post(url, data='', verify=VERIFY_CERTIFICATE)
 
     if response.status_code == 201:
         json_response = response.json()
@@ -47,11 +52,20 @@ def perform_post_request(filename):
                 print(f"Extracted 'artifactId': {artifact_id}")
             else:
                 print("No 'artifactId' found in the response.")
-            return upload_url
+            return upload_url,artifact_id
         else:
             print("No 'uploadUrl' found in the response.")
     else:
         print(f"Failed to make the POST request. Status Code: {response.status_code}")
+
+def perform_post_scan_request(artifact_id):
+    url = f"http://localhost:5010/v1/scan/{artifact_id}"
+    response = requests.post(url,verify=VERIFY_CERTIFICATE)
+
+    if response.status_code == 202:
+        print("Scan request successful.")
+    else:
+        print(f"Failed to perform scan request. Status Code: {response.status_code}")
 
 if __name__ == "__main__":
     num_iterations = 10  # Adjust the number of iterations here
@@ -63,16 +77,19 @@ if __name__ == "__main__":
         csv_file_path = generate_csv_file(csv_file_path, num_rows, num_columns)  # Create and save the CSV file
         print(f"CSV file with row headers and columns created and saved as '{csv_file_path}'")
 
-        upload_url = perform_post_request(csv_file_path)  # Get the upload URL from the POST request
+        upload_url, artifact_id = perform_post_request(csv_file_path)  # Get the upload URL and artifactId
         if upload_url:
             with open(csv_file_path, 'r') as csv_file:
                 csv_content = csv_file.read()
 
             headers = {'Content-Type': 'text/plain'}
-            response = requests.put(upload_url, headers=headers, data=csv_content)
+            response = requests.put(upload_url, headers=headers, data=csv_content,verify=VERIFY_CERTIFICATE)
 
             if response.status_code == 200:
                 print(f"CSV file uploaded successfully")
+                print(f"Extracted 'artifactId': {artifact_id}")
+                #NOTE: Uncomment the below line to perform the scan request locally
+                perform_post_scan_request(artifact_id)  # Perform POST request for scan
             else:
                 print(f"Failed to upload CSV file. Status Code: {response.status_code}")
 
